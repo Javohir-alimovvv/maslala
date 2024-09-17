26bf788e76faf19f52116805da301fd8e809954d
javohir <ajavohir969@gmail.com>
2024-09-17 23:31:02 +0500

    masala

---
+++ js/app.js
@@ -0,0 +1,136 @@
+
+// 1#
+// {
+    // nums = [2, 7, 11, 15], target = 9 => ixtiyoriy array va biron bir target nomli
+//  o'zgaruvchi olinsin. Array ichidagi sonlar yig'indisi target ga teng bulgan sonlarni index si topilsin
+// Misol uchun 2 va 7 soni yig'indisi 9 ga teng, masala sharti mana shu 2 va 7 soni indexi console ga chiqarilsin
+// }
+
+// {
+//     let arr = [3,8,7,1,5,4]
+//     // console.log(arr);
+    
+//     let target = 9
+
+//     let resalt = []
+//     for(let i  = 0; i < arr.length; i++){
+//         console.log(arr[i]);
+//         for(let b = 0; b < i; b++){
+//             if(arr[i] + arr[b] === target){
+//                 resalt.push(b, i)
+//             }
+//         }
+//     }
+    
+// console.log(resalt)
+    
+// }
+
+
+// 2#
+// {
+    // Ixtiyoriy n ta element dan iborat bo’lgan array yarating va uning ichidagi dublicate bo’lgan item larni o’chiring:
+    // let arr = [1, 3, 2, 2, 3 ] ⇒ 2 va 3 1 martadan ko’p ishtirok etgani uchun ularni o’chirish kerak
+    // natija ⇒ [1,2,3]
+// }
+
+// {
+//     let arr = [1,8,3,4,1,3,6]
+//     let resalt = []
+//     for(let i = 0; i < arr.length; i++){
+//         if(!resalt.includes(arr[i])){
+//             resalt.push(arr[i])
+//         }
+        
+//     }
+//     console.log(resalt)
+// }
+
+
+// 3#
+// {
+//     n natural soni berilgan 2 sonining dastlabki n ta darajasidan tashkil topgan arrayni qaytaruvchi getLevel(n) nomli function tuzing
+//     n: 4 
+//     result : [2,4,8,16] 2 ning 1-darajasidan boshlanishi kerak va 4-darajasigacha
+//     n: 5
+//     result: [2,4,8,16,32] 2 ning 5-darajasi 32 bo’ganligi uchun 32 da tugayapti
+// }
+
+
+// {
+//     function getLevel(son, daraja){
+//         console.log(Math.pow(son, daraja))
+//     }    
+
+//     getLevel(2,5);
+// }
+
+
+// 4#
+
+// {
+//     Ixtiyoriy arrow function yarating va unga 
+//     beriladigan argument promt yordamida kiritilgan son bo'lsin.
+//     Kiritilgan sonning raqamlari yig'indisi topilsin.
+// }
+
+{
+    const num = () => {
+        
+    }
+}
+
+
+// 5#
+
+{
+    // Ixtiyoriy arrow function yarating va uning parametriga bir nechta sonlar berilsin.
+    // va o'sha sonlar yig'indisini arrayning element lari ko'rinishida ifodalang:
+    // masalan: [1, 22, 3] yigindisi: 26
+    // result=> [2,6]
+}
+
+{
+    const yigindi = () => {
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
